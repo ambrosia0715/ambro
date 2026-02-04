84fd79700850e814b874d73f1a61c2d9661d2a0d
ambrosia0715 <ambrosia0715@gmail.com>
2026-02-04 09:55:53 +0900

    ads.txt 추가: 웹 광고 인증용 (Google AdSense), vercel rewrite 설정

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,11 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "d74f4dfa21118a94dc66e56fc665e84f",
+const RESOURCES = {"flutter_bootstrap.js": "38f2ea3c960dca66415d93111f99b3be",
 "version.json": "cdf04dffada93455ab97eeb869fb3c31",
 "index.html": "b2c35c79778a971c32f10c111ca03785",
 "/": "b2c35c79778a971c32f10c111ca03785",
 "main.dart.js": "a26207c8fc3e11514826f7ace7234a7a",
+"ads.txt": "d23943dcf94ceab2656e40f349acbc8c",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "app-ads.txt": "cfcf8b649ad157aa579d90769dc3d84d",
 "favicon.png": "d0c59f46dfdda82783cbf4f554571279",
